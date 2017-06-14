9f462d3389a8cecfbe9e6c8437e14559b15b9095
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-14 12:54:15 +0200

    add docstrings in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from ship import Ship
 import csv
 def show_screen(filename):
+    """
+    Prints screen from a csv file.
+    Parameters:
+    ----------
+    filename: str
+    """
 
     with open(filename, "r") as hello_screen_file:
         hello_screen_reader = csv.reader(hello_screen_file)
@@ -9,6 +15,17 @@ def show_screen(filename):
             print(*line)
 
 def define_ship_type(ship_types):
+    """
+    Get ship type from a user and returns it.
+    Parameters:
+    ----------
+    ship_types: list
+
+    Returns:
+    -------
+    user_ship_type: str
+    ship_types: list of str
+    """
 
     user_ship_type = input("\nEnter a ship type: ").title()
 
@@ -24,6 +41,12 @@ def define_ship_type(ship_types):
     return user_ship_type, ship_types
 
 def define_ship_coordinates():
+    """
+    Gets coordinates from a user.
+    Returns:
+    -------
+    coordinates: tuple
+    """
     user_row_number = int(input("Enter a row number: "))
     user_column_number = int(input("Enter a column number: "))
     coordinates_range = range(1,11)
@@ -36,8 +59,13 @@ def define_ship_coordinates():
     return coordinates
 
 def define_ship_turn():
-
-    user_turn = input("Do you want to place the ship horizontally (put 'h') or vertically? (put 'v') ").lower()
+    """Gets a turn of a placement of ship from a user.
+    Returns:
+    -------
+    user_turn: bool
+    """
+    user_turn = input("Do you want to place the ship horizontally (put 'h')" +
+                      " or vertically? (put 'v') ").lower()
 
     if user_turn == "h":
         user_turn = True
@@ -49,6 +77,14 @@ def define_ship_turn():
     return user_turn
 
 def define_ship_placement(ship_types):
+    """
+    Gets all information about ship placement.
+    Returns:
+    -------
+    user_ship_type: str
+    coordinates: tuple
+    user_turn: bool
+    """
 
     show_ship_types(ship_types)
     user_ship_type, ship_types = define_ship_type(ship_types)
@@ -59,11 +95,20 @@ def define_ship_placement(ship_types):
 
 
 def show_ship_types(ship_types):
+    """
+    Prints ship types from a ship_types list.
+    Parameters:
+    ----------
+    ship_types: list of str
+    """
     print("\nYou can create one of each type of ships: ")
     for ship_type in ship_types:
         print(ship_type)
 
 def main():
+    """
+    Handles game.
+    """
     RED = '\033[91m'
     WHITE = '\033[0m'
     show_screen("hello_screen.csv")
