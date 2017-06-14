740185c998a8039f86459ae5dcd838e15b787344
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-06-14 10:14:02 +0200

    (ocean.py) handle type and value errors

--- ocean.py
+++ ocean.py
@@ -17,12 +17,16 @@ class Ocean:
 
     def add_to_ocean(self, coord, item):
 
-        self.board[coord] = item
+        if type(coord) == "tuple":
+            if coord[0] and coord[1] in range(1, 11):
+                self.board[coord] = item
+            else:
+                raise ValueError("Coordinates do not fit on the board (10x10).")
+        else:
+            raise TypeError("Invalid coordinate type (expected tuple).")
 
     def add_water(self):
 
         for coordinate in self.coordinates:
             if coordinate not in list(self.board.keys()):
                 self.board[coordinate] = Water()
-
-x = Ocean()
