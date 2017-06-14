12d3906ffbcad06ba7bb8e3d76334f76ac572d20
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-06-14 09:41:10 +0200

    (ocean.py) fill board with ships and water

--- ocean.py
+++ ocean.py
@@ -1,9 +1,9 @@
 class Ocean:
 
-    def __init__(self, ship_coords):
-        self.ship_coords = ship_coords
+    def __init__(self):
         self.is_hidden = False
         self.coordinates = ()
+        self.board = {}
 
         rows = range(1, 11)
         columns = []
@@ -12,3 +12,13 @@ class Ocean:
             columns.append(row)
             for column in columns:
                 coordinates.append((column, row))
+
+    def add_to_ocean(self, coord, item):
+
+        self.board[coord] = item
+
+    def add_water(self):
+
+        for coordinate in self.coordinates:
+            if coordinate not in self.board.keys():
+                board[coordinate] = Water()
