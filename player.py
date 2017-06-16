c1c29ffb6ab83f17dd2d77d4b5d02b94e57ffe9d
Ziemo Andrzejewski <andrzejewski.ziemowit@gmail.com>
2017-06-16 10:11:08 +0200

    refactored player

--- player.py
+++ player.py
@@ -13,8 +13,10 @@ class Player:
         self.init_ships_dict()
         self.sunken_ships = []
 
+
     def append_sunken_ship(self, hit_object):
         self.sunken_ships.append(hit_object)
+
 
     def init_ships_dict(self):
 
@@ -25,22 +27,26 @@ class Player:
         self.ship_dict['Submarine'] = Ship(3)
         self.ship_dict['Destroyer'] = Ship(2)
 
+
     def add_ship_to_ocean(self, ship_name, coordinates, turn=True):
 
         if ship_name not in self.ship_dict.keys():
             raise NameError('Invalid ship name')
+
         ship = self.ship_dict[ship_name]
         ship_lenght = len(ship.square_list)
         column = coordinates[0]
         row = coordinates[1]
         self.check_range(column, row, ship_lenght, turn)
         self.check_is_water(column, row, ship_lenght, turn)
+
         for value in range(ship_lenght):
             square = ship.square_list[value]
             if turn is True:
                 self.player_ocean.add_to_ocean((column+value, row), square)
             elif turn is False:
                 self.player_ocean.add_to_ocean((column, row+value), square)
+
 
     def check_is_water(self, column, row, ship_lenght, turn):
 
@@ -53,9 +59,11 @@ class Player:
                     if self.player_ocean.board.get((column+x, row+y)) is not None:
                         raise KeyError
 
+
     def check_range(self, column, row, ship_lenght, turn):
 
         max_value = 10
+
         if turn is True:
             if column + ship_lenght > max_value:
                 raise KeyError
@@ -63,6 +71,7 @@ class Player:
             if row + ship_lenght > max_value:
 
                 raise KeyError
+
 
     def shoot_ship(self, enemy, shooted_object):
 
@@ -76,14 +85,17 @@ class Player:
                     ship.mark_square(item_index)
                     return key
 
+
     def _copy_object(self, object_to_shoot, shoot_coordinates):
 
         self.enemy_ocean_representation.add_to_ocean(shoot_coordinates, object_to_shoot)
+
 
     def shoot_and_check_if_is_sunk(self, enemy, shoot_coordinates):
 
         object_to_shoot = enemy.player_ocean.get_item_from_ocean(shoot_coordinates)
         self._copy_object(object_to_shoot, shoot_coordinates)
+        
         if type(object_to_shoot) is Square:
             shooted_object_name = self.shoot_ship(enemy, object_to_shoot)
         elif type(object_to_shoot) is Water:
