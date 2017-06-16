4d016ffe8391a3ff025eb01968b5e45852902afe
Ziemo Andrzejewski <andrzejewski.ziemowit@gmail.com>
2017-06-16 10:26:28 +0200

    refactored player

--- player.py
+++ player.py
@@ -7,6 +7,19 @@ from water import Water
 class Player:
 
     def __init__(self, name):
+        """
+        Constructs a *Player* object.
+
+        Parameters:
+        ----------
+        name: str
+        enemy_ocean_representation: object
+        player_ocean: object
+        init_ships_dict: dict
+        sunken_ships: list
+        ------
+        """
+
         self.name = name
         self.enemy_ocean_representation = Ocean()
         self.player_ocean = Ocean()
@@ -15,10 +28,27 @@ class Player:
 
 
     def append_sunken_ship(self, hit_object):
+        """
+        Appends ship in specific coordinates and turn to ocean object.
+
+        Parameters:
+        ----------
+        ship_name: str
+   	    coordinates: tuple of integers
+   	    turn: bool
+        ------
+        """
+
         self.sunken_ships.append(hit_object)
 
 
     def init_ships_dict(self):
+        """
+        Creates dictionary: keys - ship's name, values - ship's object.
+
+        Parameters:
+        ----------
+        """
 
         self.ship_dict = {}
         self.ship_dict['Carrier'] = Ship(5)
@@ -29,6 +59,16 @@ class Player:
 
 
     def add_ship_to_ocean(self, ship_name, coordinates, turn=True):
+        """
+        Appends ship in specific coordinates and turn to ocean object.
+
+        Parameters:
+        ----------
+        ship_name: str
+   	    coordinates: tuple of integers
+   	    turn: bool
+        ------
+        """
 
         if ship_name not in self.ship_dict.keys():
             raise NameError('Invalid ship name')
@@ -49,6 +89,16 @@ class Player:
 
 
     def check_is_water(self, column, row, ship_lenght, turn):
+        """
+        Parameters:
+        ----------
+        column, row, ship_length: int
+	    turn: bool
+
+        Raises:
+        ------
+        KeyError: when user tries to add ship on another ship or next to.
+        """
 
         for x in range(-1, 2):
             for y in range(-1, ship_lenght+1):
@@ -61,6 +111,16 @@ class Player:
 
 
     def check_range(self, column, row, ship_lenght, turn):
+        """
+        Parameters:
+        ----------
+        column, row, ship_length: int
+	    turn: bool
+
+        Raises:
+        ------
+        KeyError: when user wants to add ship out of edge
+        """
 
         max_value = 10
 
@@ -74,6 +134,17 @@ class Player:
 
 
     def shoot_ship(self, enemy, shooted_object):
+        """
+        Checks what kind of object was shooted (square or water).
+
+        Parameters:
+        ----------
+        enemy: object
+   	    shooted_object: object
+
+        Returns: key - str
+        ------
+        """
 
         dict_of_ships = enemy.ship_dict
         for key in dict_of_ships:
@@ -87,15 +158,34 @@ class Player:
 
 
     def _copy_object(self, object_to_shoot, shoot_coordinates):
+        """
+        Double representation of player's board.
+
+        Parameters:
+        ----------
+        object_to_shoot: object
+   	    shoot_coordinates: tuple
+        ------
+        """
 
         self.enemy_ocean_representation.add_to_ocean(shoot_coordinates, object_to_shoot)
 
 
     def shoot_and_check_if_is_sunk(self, enemy, shoot_coordinates):
+        """
+        Checks what kind of object was shooted and returns its name.
+
+        Parameters:
+        ----------
+        enemy: object
+   	    shoot_coordinates: tuple
+
+        ------
+        """
 
         object_to_shoot = enemy.player_ocean.get_item_from_ocean(shoot_coordinates)
         self._copy_object(object_to_shoot, shoot_coordinates)
-        
+
         if type(object_to_shoot) is Square:
             shooted_object_name = self.shoot_ship(enemy, object_to_shoot)
         elif type(object_to_shoot) is Water:
